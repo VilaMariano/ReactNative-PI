e8fce618d152d6dfbeeb7005821a7303faf98ac0
margaritaziraldo <mziraldo@udesa.edu.ar>
2025-06-02 20:58:13 -0300

    estilos para profile y usuario

--- ReactNative-PI/src/components/Usuario.js
+++ ReactNative-PI/src/components/Usuario.js
@@ -4,20 +4,41 @@ import { db, auth } from '../firebase/config'
 import firebase from 'firebase'
 
 class Usuario extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
         }
     }
 
-    render(){
-        return(
-            <View>
-                <Text>{this.props.data.userName}</Text>
-                <Text>{this.props.data.email}</Text>
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.userName}>{this.props.data.userName}</Text>
+                <Text style={styles.email}>{this.props.data.email}</Text>
             </View>
-            
+
         )
-    } 
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 20,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#f9c2d1',
+        paddingBottom: 10,
+    },
+    userName: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#d63384',
+        marginBottom: 4,
+    },
+    email: {
+        fontSize: 16,
+        color: '#666',
+    },
+})
+
 export default Usuario 
--- ReactNative-PI/src/screens/Profile.js
+++ ReactNative-PI/src/screens/Profile.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, StyleSheet, FlatList} from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native'
 import React, { Component } from 'react'
 import { auth, db } from '../firebase/config'
 import Usuario from '../components/Usuario'
@@ -9,26 +9,26 @@ export default class Perfil extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      usuario : [],
+      usuario: [],
       posteos: []
     }
   }
 
   //Como recupero los datos, trabajamos con el modulo db y colleccion//
-  componentDidMount(){
+  componentDidMount() {
     db.collection('users')
-    .where('email', '==', auth.currentUser.email)
-    .onSnapshot((docs) => {
-      let arrDocs = [];
-      docs.forEach((doc) => arrDocs.push({
-        id: doc.id,
-        data: doc.data()
-      }))
-      this.setState({
-        usuario: arrDocs
-      }, 
-      () => console.log('este es el state', this.state))
-    })
+      .where('email', '==', auth.currentUser.email)
+      .onSnapshot((docs) => {
+        let arrDocs = [];
+        docs.forEach((doc) => arrDocs.push({
+          id: doc.id,
+          data: doc.data()
+        }))
+        this.setState({
+          usuario: arrDocs
+        },
+          () => console.log('este es el state', this.state))
+      })
 
     db.collection('posts')
       .where('owner', '==', auth.currentUser.email)
@@ -42,40 +42,45 @@ export default class Perfil extends Component {
           posteos: posts
         });
       });
-  }  
+  }
 
   logout() {
     auth.signOut()
-      .then(() => this.props.navigation.navigate('Login')) 
+      .then(() => this.props.navigation.navigate('Login'))
       .catch(err => console.log('err en el signout', err))
   }
 
   render() {
     return (
-      <View>
-        <Text>Profile</Text>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Mi Perfil</Text>
 
-        <FlatList
-          data={this.state.usuario}
-          keyExtractor={(item) =>  item.id.toString()}
-          renderItem = {({ item }) => <Usuario id={item.id} data={item.data} /> }
-        />
-
-        <Text>Mis posteos</Text>
-
-        {this.state.posteos.length === 0 ? 
-        (<Text>No tienes posteos aún.</Text>) : (
-        <FlatList
-         data={this.state.posteos}
-         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-        <Post id={item.id} data={item.data} />
-        )}/>)     
-        }
+        <View style={styles.section}>
+          <FlatList
+            data={this.state.usuario}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <Usuario id={item.id} data={item.data} />}
+          />
+        </View>
 
 
-        <TouchableOpacity onPress={() => this.logout()}>
-          <Text style={styles.Btn}>
+        <Text style={styles.subtitulo}>Mis posteos</Text>
+
+        <View style={styles.section}>
+          {this.state.posteos.length === 0 ?
+            (<Text style={styles.mensaje}>No tenés posteos aún.</Text>) : (
+              <FlatList
+                data={this.state.posteos}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                  <Post id={item.id} data={item.data} />
+                )} />)
+          }
+        </View>
+
+
+        <TouchableOpacity style={styles.boton} onPress={() => this.logout()}>
+          <Text style={styles.botonTexto}>
             Cerrar Sesión
           </Text>
         </TouchableOpacity>
@@ -85,8 +90,46 @@ export default class Perfil extends Component {
 
 }
 const styles = StyleSheet.create({
-    Btn: {
-        color: '#ff1493',
-        textAlign: 'center',
-        marginTop: 15
-    }})
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff0f5'
+  },
+  titulo: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#d63384',
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  subtitulo: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#c2185b',
+    marginTop: 30,
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+  mensaje: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#666'
+  },
+  section: {
+    marginBottom: 20
+  },
+  boton: {
+    backgroundColor: '#ff1493',
+    padding: 14,
+    borderRadius: 10,
+    marginTop: 30,
+    alignSelf: 'center',
+    width: '60%'
+  },
+  botonTexto: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+});
