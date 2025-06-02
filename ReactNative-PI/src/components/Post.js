abd2e24c52d98168241cc93f572bc274097576e5
margaritaziraldo <mziraldo@udesa.edu.ar>
2025-06-02 20:02:03 -0300

    estilos para feed y posteos

--- ReactNative-PI/src/components/Post.js
+++ ReactNative-PI/src/components/Post.js
@@ -6,79 +6,121 @@ import { FontAwesome } from '@expo/vector-icons'
 
 class Post extends Component {
     constructor(props) {
-            super(props)
-            this.state = {
-                likes: false,
-                cantLikes: 0,
-                posteos: [],
-            }
+        super(props)
+        this.state = {
+            likes: false,
+            cantLikes: 0,
+            posteos: [],
         }
+    }
 
     borrarPost() {
-    db.collection('posts').doc(this.props.id).delete()
-    .then(() => console.log('Eliminado'))
-    .catch(err => console.log('Error', err));
+        db.collection('posts').doc(this.props.id).delete()
+            .then(() => console.log('Eliminado'))
+            .catch(err => console.log('Error', err));
     }
 
     likearPost(id) {
-            db.collection('posts')
-                .doc(id)
-                .update({
-                    likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
-                })
-                .then(() => this.setState((prevState) => ({
-        
+        db.collection('posts')
+            .doc(id)
+            .update({
+                likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
+            })
+            .then(() => this.setState((prevState) => ({
+
                 likes: true,
-                    cantLikes: prevState.cantLikes + 1
-                })));
-        }
-    
+                cantLikes: prevState.cantLikes + 1
+            })));
+    }
+
     unlikePost(id) {
-            db.collection('posts')
-                .doc(id)
-                .update({
-                    likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
-                })
-                .then(() => this.setState((prevState) => ({
-                    likes: false,
-                    cantLikes: prevState.cantLikes - 1
-                })));
-        }
+        db.collection('posts')
+            .doc(id)
+            .update({
+                likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+            })
+            .then(() => this.setState((prevState) => ({
+                likes: false,
+                cantLikes: prevState.cantLikes - 1
+            })));
+    }
 
 
-    render(){
-        
-        return(
-            <View>
-                <Text>{this.props.data.owner}</Text>
-                <Text>{this.props.data.mensaje}</Text>
+    render() {
+
+        return (
+            <View style={styles.postContainer}>
+                <Text style={styles.owner}>{this.props.data.owner}</Text>
+                <Text style={styles.mensaje}>{this.props.data.mensaje}</Text>
                 {this.props.data.owner === auth.currentUser.email && (
-                <TouchableOpacity onPress={() => this.borrarPost()}>
-                <Text>Borrar</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.borrarPost()}>
+                        <Text style={styles.botonBorrar}>Borrar</Text>
+                    </TouchableOpacity>
                 )}
-                <View>
-				<Text>{this.props.data.likes.length}</Text>
-					{
-					this.props.data.likes.includes(auth.currentUser.email) ?
-					<FontAwesome
-					name="heart"
-					size={18}
-					color="red"
-					onPress={() => this.unlikePost(this.props.id)}
-					/> :
-					<FontAwesome
-					name="heart-o"
-					size={18}
-					color="black"
-					onPress={() => this.likearPost(this.props.id)}
-					/>
-					}
-				</View>
-									
+
+                <View style={styles.likesContainer}>
+                    <Text style={styles.likeCount}>{this.props.data.likes.length}</Text>
+                    {
+                        this.props.data.likes.includes(auth.currentUser.email) ?
+                            <FontAwesome
+                                name="heart"
+                                size={18}
+                                color="red"
+                                onPress={() => this.unlikePost(this.props.id)}
+                            /> :
+                            <FontAwesome
+                                name="heart-o"
+                                size={18}
+                                color="black"
+                                onPress={() => this.likearPost(this.props.id)}
+                            />
+                    }
+                </View>
+
             </View>
-            
+
         )
-    } 
+    }
 }
+
+const styles = StyleSheet.create({
+    postContainer: {
+        backgroundColor: '#ffe6f0',
+        padding: 16,
+        marginVertical: 10,
+        marginHorizontal: 16,
+        borderRadius: 12,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 2
+    },
+    owner: {
+        fontWeight: 'bold',
+        color: '#c2185b',
+        marginBottom: 4
+    },
+    mensaje: {
+        fontSize: 16,
+        color: '#333',
+        marginBottom: 12
+    },
+    botonBorrar: {
+        color: '#d63384',
+        fontSize: 14,
+        textAlign: 'right',
+        marginTop: 8
+    },
+    likesContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10
+    },
+    likeCount: {
+        marginRight: 8,
+        color: '#c2185b'
+    }
+});
+
 export default Post
--- ReactNative-PI/src/screens/Feed.js
+++ ReactNative-PI/src/screens/Feed.js
@@ -3,22 +3,22 @@ import React, { Component } from 'react'
 import { db, auth } from '../firebase/config'
 import firebase from 'firebase'
 import Post from '../components/Post'
-
+import { StyleSheet } from 'react-native'
 
 class Feed extends Component {
-  constructor(props) {
-            super(props)
-            this.state = {
-                cargando: false,
-                posteos: [],
-            }
-        }
+	constructor(props) {
+		super(props)
+		this.state = {
+			cargando: false,
+			posteos: [],
+		}
+	}
 
 	componentDidMount() {
-		this.setState({cargando: true})
+		this.setState({ cargando: true })
 
 		db.collection('posts')
-      .orderBy('createdAt', 'desc')
+			.orderBy('createdAt', 'desc')
 			.onSnapshot((docs) => {
 				let posts = [];
 				docs.forEach((doc) => posts.push({
@@ -26,34 +26,55 @@ class Feed extends Component {
 					data: doc.data()
 				}));
 				this.setState({
-				  	posteos: posts,
-					  cargando: false
+					posteos: posts,
+					cargando: false
 				});
 			});
 	}
 
 	render() {
 		return (
-			<View>
-				<Text>Feed</Text>
+			<View style={styles.container}>
+				<Text style={styles.titulo}>Feed</Text>
 				{
 					this.state.cargando ?
-						<Text>Cargando...</Text> :
+						<Text style={styles.mensaje}>Cargando...</Text> :
 						(
-							this.state.posteos.length === 0	?
-							<Text>No hay posteos para mostrar</Text> :
-							<FlatList
-								data={this.state.posteos}
-								keyExtractor={(item) => item.id.toString()}
-								renderItem={({ item }) => (
-									<Post data={item.data} id={item.id} />
-								)}
-							/>
+							this.state.posteos.length === 0 ?
+								<Text style={styles.mensaje}>No hay posteos para mostrar</Text> :
+								<FlatList
+									data={this.state.posteos}
+									keyExtractor={(item) => item.id.toString()}
+									renderItem={({ item }) => (
+										<Post data={item.data} id={item.id} />
+									)}
+								/>
 						)
 				}
 			</View>
 		)
 	}
 }
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 16,
+		backgroundColor: '#fff0f5'
+	},
+	titulo: {
+		fontSize: 26,
+		fontWeight: 'bold',
+		color: '#d63384',
+		textAlign: 'center',
+		marginVertical: 16
+	},
+	mensaje: {
+		fontSize: 16,
+		textAlign: 'center',
+		color: '#555'
+	}
+});
 
 export default Feed;
