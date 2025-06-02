78f5a837b6eb3d2d7a77072e02a0898ffd4605ca
margaritaziraldo <mziraldo@udesa.edu.ar>
2025-06-02 11:39:38 -0300

    remember me funciona

--- ReactNative-PI/src/navigation/StackNavigation.js
+++ ReactNative-PI/src/navigation/StackNavigation.js
@@ -1,33 +1,63 @@
-import React from "react";
+import React, {Component} from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import BottomTabs from './BottonTabs'
 import Register from "../screens/Register";
 import Login from "../screens/Login";
+import { auth } from "../firebase/config";
+
 
 const Stack = createNativeStackNavigator();
 
-function StackNavigation() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="Login"
-                component={Login}
-                options={{
-                    headerShown: false
-                }} />
-            <Stack.Screen
-                name="Register"
-                component={Register}
-                options={{
-                    headerShown: false
-                }} />
-            <Stack.Screen
-                name="Tab"
-                component={BottomTabs}
-                options={{
-                    headerShown: false
-                }} />
-        </Stack.Navigator>
-    )
+class StackNavigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            logueado: false
+        }
+    }
+    componentDidMount() {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({
+                    logueado: true
+                })
+            } else {
+                this.setState({
+                    logueado: false
+                })
+            }
+        });
+    }
+
+    render() {
+        return (
+            <Stack.Navigator>
+                {
+                    this.state.logueado === false ?
+                        <>
+                            <Stack.Screen
+                                name="Login"
+                                component={Login}
+                                options={{
+                                    headerShown: false
+                                }} />
+                            <Stack.Screen
+                                name="Register"
+                                component={Register}
+                                options={{
+                                    headerShown: false
+                                }} />
+                        </> :
+                        <Stack.Screen
+                            name="Tab"
+                            component={BottomTabs}
+                            options={{
+                                headerShown: false
+                            }} />
+                }
+            </Stack.Navigator>
+        )
+    }
 }
+
 export default StackNavigation
